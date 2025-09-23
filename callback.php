a549f521b84e928065a918ccba87b2e5a9b3ebc6
jmarlier <jeromemarlier@gmail.com>
2025-09-23 18:32:38 +0200

    gna gna gna

--- callback.php
+++ callback.php
@@ -2,6 +2,8 @@
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/php-error.log'); // 🔁 modifiable
 error_reporting(E_ALL);
 
 require __DIR__ . '/vendor/autoload.php';
